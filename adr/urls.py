bed52b3934f0c43a1fc1f5d035306d2b3ff370cf
Iquique-practicantes <iquiquepracticantes@gmail.com>
2026-03-18 14:32:35 -0300

    Mejora UI: Estilizar correo de recuperación con HTML y branding ADR

--- adr/urls.py
+++ adr/urls.py
@@ -68,7 +68,10 @@ urlpatterns = [
         name="login",
     ),
     
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        html_email_template_name='registration/password_reset_email.html',
+        subject_template_name='registration/password_reset_subject.txt',
+    ), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
