48ee957bb0f84c03c4a4e4ff867164581204422c
ipsoteric <juan.castillo18@outlook.com>
2026-05-14 20:29:16 -0400

    refactor: Traslado de rutas para recuperar contraseña de adr a accounts

--- adr/urls.py
+++ adr/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
@@ -45,7 +44,6 @@ from .forms import LoginForm
 urlpatterns = [
     # Autenticación
     #path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
     path('password_change/', login_required(ProfilePasswordChangeView.as_view()), name="profile_password_change"),
     
     # Gestión de Usuarios
@@ -55,27 +53,6 @@ urlpatterns = [
     path("mi-perfil/", views.my_profile, name="my_profile"),
     path('profile_delete/<int:pk>/', login_required(ProfileDeleteView.as_view()), name='profile_delete'),
     path("perfil/contraseña/cambiar/", UserPasswordChangeView.as_view(), name="password_change"),
-    # Páginas Principales
-    #path('', IndexView.as_view(), name="index"),
-    # Esta ruta para el home será sustituida
-    # path('inicio/', login_required(HomeView.as_view()), name='inicio'),
-    #path(
-    #    "login/",
-    #    LoginView.as_view(
-    #        authentication_form= LoginForm,
-    #        template_name="registration/login.html",
-    #    ),
-    #    name="login",
-    #),
-    
-    path('password_reset/', auth_views.PasswordResetView.as_view(
-        email_template_name='registration/password_reset_email.txt',
-        html_email_template_name='registration/password_reset_email.html',
-        subject_template_name='registration/password_reset_subject.txt',
-    ), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
    
         # Redirección LEGACY con nombre 'home' (así reverse('home') sigue funcionando)
     path('home/', RedirectView.as_view(pattern_name='inicio', permanent=True), name='home'),
