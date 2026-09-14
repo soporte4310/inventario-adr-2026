9c8e40a1707aeaa834847a3960340d011fa7a039
SebasVera06 <sebasvera006@gmail.com>
2026-09-14 16:40:28 -0300

    Se definieron las reglas de validación en validators.py y se importaron en models.py para su aplicación.

--- common/validators.py
+++ common/validators.py
@@ -1,5 +1,17 @@
 from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 
+# Validador para nombres de personas, áreas y cargos (Solo letras y espacios)
+validador_solo_letras = RegexValidator(
+    regex=r'^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$',
+    message='Error: Este campo solo admite letras y espacios. No se permiten números ni caracteres especiales (ej: #, @, !).'
+)
+
+# Validador para códigos como BDO, NetBIOS, N° de Serie (Alfanumérico sin espacios)
+validador_alfanumerico_estricto = RegexValidator(
+    regex=r'^[a-zA-Z0-9\-]+$',
+    message='Error: Este campo solo admite letras, números y guiones medios. No ingrese espacios ni caracteres especiales.'
+)
 
 def validar_extension_imagen(imagen):
     """
